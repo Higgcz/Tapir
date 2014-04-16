9e7a4aabd6aae5add7914f641f5103d80796a290
Vojtech Micka <vojtechmicka@Vojtechs-MacBook-Pro.local>
2014-04-16 15:22:46 +0200

    Workspace setup.

---
+++ Sample Tapir/Sample Tapir/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample Tapir
+//
+//  Created by Vojtech Micka on 16.04.14.
+//  Copyright (c) 2014 Vojtech Micka. All rights reserved.
+//
+
+#include <iostream>
+
+#include <TGL/TGL.h>
+#include <TSL/TSL.h>
+
+int main(int argc, const char * argv[])
+{
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
